c88d3de39ca1e9088bef1dfd516fcf588cbcb80c
seyyed hassan moosavi nazar <73283752+moosavinazar@users.noreply.github.com>
2021-12-15 20:36:16 +0330

    create observable and use map and shareReplay operators

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,53 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {Observable, shareReplay} from "rxjs";
+import {map, filter} from "rxjs/operators";
+import {Todo} from "./models/models";
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'observables2';
+export class AppComponent implements OnInit{
+
+  public http$!: Observable<Todo[]>;
+  public doneTasks$!: Observable<Todo[]>;
+  public undoneTasks$!: Observable<Todo[]>;
+
+  ngOnInit(): void {
+
+    this.http$ = new Observable<Todo[]>((observer) => {
+      fetch('https://jsonplaceholder.typicode.com/todos')
+        .then((response) => {
+          return response.json();
+        })
+        .then((body) => {
+          observer.next(body);
+          observer.complete();
+        })
+        .catch((err) => {
+          observer.error(err);
+        });
+    });
+
+    this.http$ = this.http$.pipe(
+      shareReplay()
+    );
+
+    this.doneTasks$ = this.http$.pipe(
+      map((todo) => todo.filter((todo) => todo.completed === true))
+    );
+
+    this.undoneTasks$ = this.http$.pipe(
+      map((todo) => todo.filter((todo) => todo.completed === true))
+    );
+
+    // Wrong way
+    /*this.http$.subscribe((val) => {
+      this.doneTasks = val.filter(todo => todo.completed === true);
+      this.undoneTasks = val.filter(todo => todo.completed === false);
+    });*/
+
+  }
+
 }
